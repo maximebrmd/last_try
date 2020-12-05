fdbc107d8297f4208c44238f9e1bcc2cee7983f4
Maxime BOURMAUD <maxime.bourmaud@braineet.com>
2020-12-05 18:37:43 +0100

    Feat: Setting up Google recommended architecture BLoC

---
+++ lib/src/app.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:universal_html/html.dart';
+import 'ui/movie_list.dart';
+import '../generated/l10n.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Last Try',
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      debugShowCheckedModeBanner: false,
+      supportedLocales: S.delegate.supportedLocales,
+      locale: kIsWeb
+          ? Locale.fromSubtags(
+              languageCode: Intl.canonicalizedLocale(window.navigator.language))
+          : null,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: Scaffold(
+        body: MovieList(),
+      ),
+    );
+  }
+}
